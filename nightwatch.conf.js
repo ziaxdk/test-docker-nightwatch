3b7caf956d33179cea9f8efd7ef3b3d38f4ff283
Kenneth Olsen <keols@dfds.com>
2018-02-21 11:00:34 +0100

    initial

---
+++ nightwatch.conf.js
@@ -0,0 +1,4 @@
+module.exports = (function(settings) {
+  settings.test_workers = true;
+  return settings;
+})(require('./nightwatch.json'));
---
+++ tests/dfds/dfds1.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'Demo test Google' : function (browser) {
+    browser
+      .url('https://devferry.dfdsseaways.com/')
+      .pause(1000)
+      .end();
+  }
+};
